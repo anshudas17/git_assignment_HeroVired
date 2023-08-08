f020333a5e33fe2ac0f0abdd4a249952e15d15fd
unknown <anshudas1812@gmail.com>
2023-08-08 16:41:26 +0530

    rectangle area logic added

---
+++ geometrical_calculator.py
@@ -0,0 +1,18 @@
+import math
+
+ 
+
+class GeometryCalculator:    
+
+    def calculate_rectangle_area(self, length, width): return length * width
+
+    
+
+
+calculator = GeometryCalculator()
+
+length = 10
+
+width = 6
+
+print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
